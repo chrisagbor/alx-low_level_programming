cc62e0ed4bb59fffc24799dd993744d68ca389c5
chrisagbor <christopheragbor2@gmail.com>
2022-09-08 23:12:24 -0700

    fixed Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,13 @@
  * main - print the string in the put function
  *
  * description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle"
  * return: 0
  */
 int main (void)
 {
 	
-	puts("\"Programming is like building a multilingual puzzle);
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	'return' (0);
 
 }	
