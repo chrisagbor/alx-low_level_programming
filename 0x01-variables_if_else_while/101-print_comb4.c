da743c0f5ec3d266e1441bd63df9e7607465c28d
chrisagbor <christopheragbor2@gmail.com>
2022-09-09 10:04:07 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,29 +14,29 @@ int main(void)
 	g = 48;
 
 	while (e < 58)
-	{		
-		
+	{
+
 		i = 48;
 		while (i < 58)
-		{	 
-			
+		{
+
 			g = 48;
 			while (g < 58)
 			{
-				
+
 				if (e != i && e != g && i != g && e < i && i < g)
 				{
 	                		putchar(e);
 	 				putchar(i);
 					putchar(g);
 					if (i ==56 && e ==55 && g ==57)
-		   			{			
+		   			{
 						break;
-					}	
+					}
 					putchar(',');
 					putchar(' ');
-				
-				
+
+
 				}
 				g++;
 			}
