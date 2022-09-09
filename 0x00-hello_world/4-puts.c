c9a34a909249fb48dd3154c6d1f577782a5fe47e
chrisagbor <christopheragbor2@gmail.com>
2022-09-08 23:25:06 -0700

    PUTS.C CORRECTED

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *
  * description: using the main function
  * this program prints "Programming is like building a multilingual puzzle"
- * return: 0
+ * return: Always 0 (Sucess)
  */
-int main (void)
+int main(void)
 {
 	
 	puts("\"Programming is like building a multilingual puzzle");
