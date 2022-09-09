e339441eedb51c6417c546a89188b9f92c163b2c
chrisagbor <christopheragbor2@gmail.com>
2022-09-09 05:30:15 -0700

    fixed 3-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,25 +3,22 @@
 /**
  * main - print the letters of the alphabet
  *
- * Description: print the letters of the alphabet except e, q
+ * Description: print the letters of the alphabet except e and q
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i = 97;
+	int i;
 
-	while (i < 123)
+	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
-		{  
-			putchar(i);
-		}
-		i++;
-			
-	}		
-	putchar(10);
-
+			{
+				putchar(i);
+			}
+	}
+	putchar('\n');
 	return (0);
 }
