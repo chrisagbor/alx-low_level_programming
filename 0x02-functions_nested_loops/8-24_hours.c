73517358e9e09e9d046ae9b15a51d772535d2ccc
chrisagbor <christopheragbor2@gmail.com>
2022-09-13 10:39:06 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,8 +19,8 @@ void jack_bauer(void)
 	mins_reminder =minutes % 10;
 	hours_reminder = hours % 10;
 	_putchar(hours / 10 + '0');
-	_putchar(hours_reminder +'0')
-	_putchar(';'0);
+	_putchar(hours_reminder + '0')
+	_putchar(';' 0);
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_reminder + '0');
 	minutes++;
