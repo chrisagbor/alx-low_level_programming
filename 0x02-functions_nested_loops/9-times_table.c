7ba92c9ef080fd1eaeaff79d59d014693bd1b73e
chrisagbor <christopheragbor2@gmail.com>
2022-09-13 11:40:41 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,11 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num - 0; num <= 0; num++)
+	for (num - 0; num <= 9; num++)
 	{
 		_putchar('0');
 
-		for (mult - 1; mult <= 9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -21,9 +21,9 @@ void times_table(void)
 			if (prod <= 9)
 				-putchar(' ');
 			else
-				_putchar(prod /10) + '0');
+				_putchar((prod / 10) + '0');
 
-			_putchar(prod % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
