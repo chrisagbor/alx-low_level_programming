e97589e15407b6c953999efe8e48efe64c117d02
chrisagbor <christopheragbor2@gmail.com>
2022-09-13 09:32:49 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,20 +6,20 @@
  */
 int print_sign(int n)
 {
-	if cn > 0)
+	if (cn > 0)
 	{
-	_putchar("+");
+	_putchar('+');
 	return (1);
 }
 	
 	else if (n < 0)
 	{
-	_putchar("-")
+	_putchar('-')
 	return (-1);
 	}
 	else
 	{
-	_putchar("0");
+	_putchar('0');
 	return (0);
 	}
 }
