bba3ffdcfda5f0d00ca9eb9fb0c9d38d2e06ea29
chrisagbor <christopheragbor2@gmail.com>
2022-09-21 09:50:28 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @dest: String that will be appended
  * @src: String to be concatenated upon
  *
- * Return: returns pointer to @dest
+ * Return: returns poiner to @dest
  */
 
 char *_strcat(char *dest, char *src)
