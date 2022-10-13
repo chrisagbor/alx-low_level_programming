7f7846f03a97cdf9710d0d84839ca46374ab96f0
chrisagbor <christopheragbor2@gmail.com>
2022-10-13 09:50:27 -0700

    fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,4 +19,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /* _VARIADIC_FUNCTION_H_ */
+#endif /*_VARIADIC_FUNCTION_H_*/
