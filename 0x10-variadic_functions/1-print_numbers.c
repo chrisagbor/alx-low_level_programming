490fe3fb4a84ebc6bd6b4d535dc5cda3b952278f
chrisagbor <christopheragbor2@gmail.com>
2022-10-13 09:43:47 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(valist, int);
-		printf("%", num);
+		printf("%d", num);
 		if (i < n - 1 && separator)
-			printf("%", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(valist);
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(valist, char *);
 		if (str)
-			printf("%", str);
+			printf("%s", str);
 		else
 			printf("(nil)");
 		if (i < n - 1 && separator)
