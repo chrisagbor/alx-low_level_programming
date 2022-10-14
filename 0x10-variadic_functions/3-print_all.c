bd89fd27d709a64cf9bcfeae45aebe9ef48fdb8a
chrisagbor <christopheragbor2@gmail.com>
2022-10-13 22:01:46 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,9 +19,9 @@ void print_all(const char * const format, ...)
 	while (format && format[i])
 	{
 		j = 0;
-		while (t_arg[j])
+		while (t_srg[j])
 		{
-			if (format[i] == t_arg[j] && c)
+			if (format[i] == t_srg[j] && c)
 			{
 				printf(",");
 				break;
@@ -29,16 +29,16 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-		case "c";
+		case 'c';
 			printf("%c", va_arg(valist, int)), c = 1;
 			break;
-		case "i";
+		case 'i';
 			printf("%d", va_arg(valist, int)), c = 1;
 			break;
-		case "f";
+		case 'f';
 			printf("%f", va_arg(valist, double)), c = 1;
 			break;
-		case "s";
+		case 's';
 			str = va_arg(valist, char *), c = 1;
 			if (!str)
 			{
