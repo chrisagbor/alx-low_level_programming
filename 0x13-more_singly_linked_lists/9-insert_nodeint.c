0288dabbe639c394d6b552fd412f2ed9361a3acb
chrisagbor <christopheragbor2@gmail.com>
2022-10-24 09:06:11 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  *        or NULL - if function fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{	
+{
 listint_t *new, *temp = *head;
 unsigned int node;
 
